12b77d14402b674ecb3e40b72f8f25ecee4cd820
Arun Kumar R <aruntutter19@gmail.com>
2023-10-04 23:10:10 +0530

    hero component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
 import myContext from "../../context/data/myContext";
+import HeroSection from "../../components/heroSection/HeroSection";
 
 function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <HeroSection />
+    </Layout>
+  );
 }
 
 export default Home;
