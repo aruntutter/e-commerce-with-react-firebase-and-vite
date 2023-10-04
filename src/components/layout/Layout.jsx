3ac1d1f643253b7e8629716db9881e4fac509392
Arun Kumar R <aruntutter19@gmail.com>
2023-10-04 13:32:58 +0530

    Initial folder setup

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../footer/Footer";
+import Navbar from "../navbar/Navbar";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <div className="content">{children}</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
