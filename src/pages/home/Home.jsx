900297a7e6789548ec0a1fb24d1d4a004e0691ed
Arun Kumar R <aruntutter19@gmail.com>
2023-10-04 23:24:39 +0530

    filter component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
-import Layout from "../../components/layout/Layout";
 import myContext from "../../context/data/myContext";
+import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
+      <Filter />
     </Layout>
   );
 }
