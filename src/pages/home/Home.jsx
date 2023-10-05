3bf8b2624fbafb4f2bfed003da55ef63edca9cf7
Arun Kumar R <aruntutter19@gmail.com>
2023-10-05 12:00:02 +0530

    productCard component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import myContext from "../../context/data/myContext";
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Filter from "../../components/filter/Filter";
+import ProductCard from "../../components/productCard/ProductCard";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
       <Filter />
+      <ProductCard />
     </Layout>
   );
 }
