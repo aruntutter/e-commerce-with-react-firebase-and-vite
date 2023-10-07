b59f1abae0e6eda77ad5b4832c184d162a122dde
Arun Kumar R <aruntutter19@gmail.com>
2023-10-07 20:22:13 +0530

    login authentication created

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,8 +1,46 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import myContext from "../../context/data/myContext";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebaseConfig";
+import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 
 const Login = () => {
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const login = async () => {
+    setLoading(true);
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      toast.success("Login Successful", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      localStorage.setItem("user", JSON.stringify(result));
+      navigate("/");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className=" flex justify-center items-center h-screen">
+      {loading && <Loader />}
       <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
         <div className="">
           <h1 className="text-center text-white text-xl mb-4 font-bold">
@@ -12,6 +50,8 @@ const Login = () => {
         <div>
           <input
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Email"
@@ -20,12 +60,17 @@ const Login = () => {
         <div>
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Password"
           />
         </div>
         <div className=" flex justify-center mb-3">
-          <button className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg">
+          <button
+            onClick={login}
+            className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg"
+          >
             Login
           </button>
         </div>
