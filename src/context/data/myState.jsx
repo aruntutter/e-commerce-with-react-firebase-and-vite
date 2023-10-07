29a29d1f2554db07b5077015f68c4496dc9fa831
Arun Kumar R <aruntutter19@gmail.com>
2023-10-08 00:50:35 +0530

    addProduct Fun() and getProduct Fun() added

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,5 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MyContext from "./myContext";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { fireDB } from "../../firebase/firebaseConfig";
+import { toast } from "react-toastify";
 
 const MyState = (props) => {
   const [mode, setMode] = useState("light");
@@ -15,8 +25,86 @@ const MyState = (props) => {
 
   const [loading, setLoading] = useState(false);
 
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  });
+
+  // Add Product Section
+  const addProduct = async () => {
+    if (
+      products.title == null ||
+      products.price == null ||
+      products.imageUrl == null ||
+      products.category == null ||
+      products.description == null
+    ) {
+      return toast.error("Please fill all fields");
+    }
+
+    setLoading(true);
+
+    try {
+      const productRef = collection(fireDB, "products");
+      await addDoc(productRef, products);
+      toast.success("Product Added successfully");
+      getProductData();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const [product, setProduct] = useState([]);
+
+  // Get product
+  const getProductData = async () => {
+    setLoading(true);
+    try {
+      const q = query(collection(fireDB, "products"), orderBy("time"));
+
+      const data = onSnapshot(q, (QuerySnapshot) => {
+        let productArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productArray.push({ ...doc.data(), id: doc.id });
+        });
+        setProduct(productArray);
+        setLoading(false);
+      });
+      return () => data;
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
   return (
-    <MyContext.Provider value={{ mode, toggleMode, loading, setLoading }}>
+    <MyContext.Provider
+      value={{
+        mode,
+        toggleMode,
+        loading,
+        setLoading,
+        products,
+        setProducts,
+        addProduct,
+        product,
+      }}
+    >
       {props.children}
     </MyContext.Provider>
   );
