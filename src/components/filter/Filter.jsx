8f82a41587530b9e012e188a344450643615a515
Arun Kumar R <aruntutter19@gmail.com>
2023-10-16 14:05:00 +0530

    admin updated

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -92,12 +92,12 @@ function Filter() {
         </div>
         <div className="flex items-center justify-between mt-4">
           <p className="font-medium">Filters</p>
-          <button
+          {/* <button
             className="px-4 py-2 bg-gray-50hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md"
             style={{ color: mode === "dark" ? "white" : "" }}
           >
             Reset Filter
-          </button>
+          </button> */}
         </div>
         <div>
           <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
